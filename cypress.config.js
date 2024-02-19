4872cfd099df1599e0d4e2f40639a3065ff92c0a
usama44c <usama44c@gmail.com>
2024-02-19 15:36:09 +0500

    config file

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
 
     baseUrl: 'https://magento.softwaretestingboard.com/',
 
-    viewportHeight: "720",
-    viewportWidth: "1020",
+    viewportHeight: 720,
+    viewportWidth: 1020,
 
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',    
     supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
